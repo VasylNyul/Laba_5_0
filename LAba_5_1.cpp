5d3e4e8dc1b7ad0eec924168ca18895302d26451
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-10-21 17:10:49 +0300

    wawwww

--- LAba_5_1.cpp
+++ LAba_5_1.cpp
@@ -10,7 +10,7 @@ int main()
  double t, s;
 
    cout << "t = ";cin >> t;
-   //cout << "s = ";cin >> s;
+   cout << "s = ";cin >> s;
     
     double c = (f(1,t*t,s)+f(t,s*s,1))/(1+f(1,t*s,1)*f(1,t*s,1));
     cout << "c = " << c << endl;
@@ -19,5 +19,5 @@ int main()
 }
 double f(const double a, const double b, const double c) // визначення
 {
-    return ( sin(a*b*c)*sin(a*b*c)/(a*a+b*b+c*c));
+    return (sin(a*b*c)*sin(a*b*c)/(a*a+b*b+c*c));
 }
